89baa81a871053e05fdae8ba2429749a918ee5c8
Cooltor <romaindubus86@gmail.com>
2022-11-02 11:25:31 +0100

    changement etat des commandes

--- admin/gestion_commandes.php
+++ admin/gestion_commandes.php
@@ -17,51 +17,92 @@ if(!userIsAdmin()){
 
 $res = $pdo->query("SELECT * FROM commande");
 
-echo '<table class=" table table-striped" border=\'2\'><tr>';
+//echo '<table class=" table table-striped" border=\'2\'><tr>';
 
-if(isset($_GET['action']) && $_GET['action'] == 'supprimer')
+//if(isset($_GET['action']) && $_GET['action'] == 'supprimer')
+//{
+//    if($pdo->query("DELETE FROM commande WHERE id_commande = '$_GET[id_commande]'")){
+ //   
+//    header ('location:gestion_commandes.php');
+//    $content .= '<div class="alert alert-success" role="alert">
+//    Cette commande a bien été supprimé
+//    </div>';
+//    }
+//}
+
+
+
+//for($i = 0; $i < $res->columnCount(); $i++)
+//{
+//    $colonne = $res->getColumnMeta($i);
+//    echo "<th>".$colonne['name']."</th>";
+//}
+
+//echo "<th>Supprimer</th>";
+
+//echo '</tr>';
+
+//while($ligne = $res->fetch(PDO::FETCH_ASSOC))
+//{
+//    echo '<tr>';
+//        foreach($ligne as $key=>$info) {
+//            
+//                echo "<td>$info</td>";
+//            
+ //       }
+
+//    echo '<td><a href="?action=supprimer&id_commande='.$ligne['id_commande'].'">Supprimer<i class="fas fa-trash-alt"></i></a></td>';
+
+//}
+
+//$vuTable = '</table>';
+
+
+if(isset($_POST['statut_cmd'])){
+
+    $pdo->query("UPDATE commande SET etat = '$_POST[etat]' WHERE id_commande = '$_POST[id_commande]'");
+}
+
+$allOrder = $pdo->query('SELECT * FROM commande ORDER BY id_commande DESC');
+
+$content .= '<table class="table table-striped" border="2"><tr>';
+
+for($i = 0; $i < $allOrder->columnCount(); $i++)
 {
-    if($pdo->query("DELETE FROM commande WHERE id_commande = '$_GET[id_commande]'")){
+    $colonne = $allOrder->getColumnMeta($i);
+    $content .= "<th>".$colonne['name']."</th>";
+}
+
+while ($oneOrder = $allOrder-> fetch(PDO::FETCH_ASSOC)) {
+
+    $content.= '<tr>';
+
+    foreach ($oneOrder as $key => $value) {
+
     
-    header ('location:gestion_commandes.php');
-    $content .= '<div class="alert alert-success" role="alert">
-    Cette commande a bien été supprimé
-    </div>';
+    if ($key == 'etat') {
+        $content .= '<form action="" method="POST">';
+        $content .= "<input type=\"hidden\" name=\"id_commande\" value=\"$oneOrder[id_commande]\">";
+        $content .= "<td class=\"align-middle\">";
+        $content .= "<select name=\"etat\" id=\"etat\">";
+        $content .= "<option value=\"$value\">$value</option>";
+        $content .= "<option value=\"envoyé\">Envoyé</option>";
+        $content .= "<option value=\"livré\">Livré</option>"; 
+        $content .= "</select>";
+        $content .= "<input type=\"submit\" value=\"Modifier\" name=\"statut_cmd\" class=\"btn btn-outline-primary btn sm-ms-2\" >";
+        $content .= "</form>";
+
+
+    }else{
+        $content .= "<td class=\"align-middle\">$value</td>";
     }
 }
-
-for($i = 0; $i < $res->columnCount(); $i++)
-{
-    $colonne = $res->getColumnMeta($i);
-    echo "<th>".$colonne['name']."</th>";
 }
-
-echo "<th>Supprimer</th>";
-echo "<th>Modifier</th>";
-echo '</tr>';
-
-while($ligne = $res->fetch(PDO::FETCH_ASSOC))
-{
-    echo '<tr>';
-        foreach($ligne as $key=>$info) {
-            
-                echo "<td>$info</td>";
-            
-        }
-
-    echo '<td><a href="?action=supprimer&id_commande='.$ligne['id_commande'].'">Supprimer<i class="fas fa-trash-alt"></i></a></td>';
-    echo '<td><a href="?action=modifier&id_commande='.$ligne['id_commande'].'">Modifier<i class="fas fa-edit"></i></a></td>';
-}
-
-$vuTable = '</table>';
-
-
-
 ?>
 
 <h1 class="text-center"> Listes de commandes passées</h1>
 
-<?php echo $vuTable; 
+<?php //echo $vuTable; 
     echo $content; ?>
 
 
