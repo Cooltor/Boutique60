2dc8e91592ed4e8113a38a2079d5b5acc579e143
Cooltor <romaindubus86@gmail.com>
2022-11-16 11:26:22 +0100

    fin

--- admin/gestion_commandes.php
+++ admin/gestion_commandes.php
@@ -17,46 +17,6 @@ if(!userIsAdmin()){
 
 $res = $pdo->query("SELECT * FROM commande");
 
-//echo '<table class=" table table-striped" border=\'2\'><tr>';
-
-//if(isset($_GET['action']) && $_GET['action'] == 'supprimer')
-//{
-//    if($pdo->query("DELETE FROM commande WHERE id_commande = '$_GET[id_commande]'")){
- //   
-//    header ('location:gestion_commandes.php');
-//    $content .= '<div class="alert alert-success" role="alert">
-//    Cette commande a bien été supprimé
-//    </div>';
-//    }
-//}
-
-
-
-//for($i = 0; $i < $res->columnCount(); $i++)
-//{
-//    $colonne = $res->getColumnMeta($i);
-//    echo "<th>".$colonne['name']."</th>";
-//}
-
-//echo "<th>Supprimer</th>";
-
-//echo '</tr>';
-
-//while($ligne = $res->fetch(PDO::FETCH_ASSOC))
-//{
-//    echo '<tr>';
-//        foreach($ligne as $key=>$info) {
-//            
-//                echo "<td>$info</td>";
-//            
- //       }
-
-//    echo '<td><a href="?action=supprimer&id_commande='.$ligne['id_commande'].'">Supprimer<i class="fas fa-trash-alt"></i></a></td>';
-
-//}
-
-//$vuTable = '</table>';
-
 
 if(isset($_POST['statut_cmd'])){
 
